5cb34ee5062ae15795628587dd3e72d825976b38
K-RLange <101263876+K-RLange@users.noreply.github.com>
2023-03-31 14:01:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'A Python package to perform unsupervised sentiment analysis using lexicon enhanced Document embeddings'
 
 
